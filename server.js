d41c9d607dc18fbd3481679c893d8c88a1d98dfb
John Drake <johnmdrake@gmail.com>
2020-01-19 05:24:43 +0000

    renamed demo_fileserver.js

--- server.js
+++ server.js
@@ -1,34 +1,20 @@
-//
-// # SimpleServer
 var http = require('http');
-var path = require('path');
+var url = require('url');
+var fs = require('fs');
 
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
-
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
-
-var express = require('express');
-var app = express();
-var util = require('util');
- 
-// app.use(express.static('controllers'));
-// app.use(express.static('models'));
-// app.use(express.static('app/views'));
-app.use('/static', express.static('app'))
-
-var server = app.listen(process.env.PORT || 8080, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-});
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  if(q.pathname == "/") filename = "./index.html";
+  console.log(filename);
+  
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
---
+++ server.old.js
@@ -0,0 +1,34 @@
+//
+// # SimpleServer
+var http = require('http');
+var path = require('path');
+
+var async = require('async');
+var socketio = require('socket.io');
+var express = require('express');
+
+//
+// ## SimpleServer `SimpleServer(obj)`
+//
+// Creates a new instance of SimpleServer with the following options:
+//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
+//
+var router = express();
+var server = http.createServer(router);
+var io = socketio.listen(server);
+
+var express = require('express');
+var app = express();
+var util = require('util');
+ 
+// app.use(express.static('controllers'));
+// app.use(express.static('models'));
+// app.use(express.static('app/views'));
+app.use('/static', express.static('app'))
+
+var server = app.listen(process.env.PORT || 8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+});
